cd1bc662f9f01b15c0773096941a64262a1b5bec
Jan Schmitz <jan.schmitz.home@googlemail.com>
2019-01-25 23:22:29 +0100

    Basic Objects

---
+++ Assets/Scripts/Entities/PickableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObject : ClickableObject
+{
+    public Sprite m_InventorySprite;
+    public int m_Uses;
+
+    public virtual void Used()
+    {
+        m_Uses--;
+        if (m_Uses == 0)
+        {
+            GameManager.Get.RemoveFromInventory(this);
+            GameManager.Get.SelectInventoryItem(null);
+        }
+    }
+}
---
+++ Assets/Scripts/TextBubbleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBubbleManager : MonoBehaviour
+{
+    public static TextBubbleManager Get { get; private set; }
+
+    private void Awake()
+    {
+        if (Get != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Get = this;
+    }
+    
+    public void DisplayTextBubble(string _text, float _displayTime)
+    {
+
+    }
+}
