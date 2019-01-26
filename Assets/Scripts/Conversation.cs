9c721d87aa2a0346a93686ff57cf4b51487bfcb4
Jan Schmitz <jan.schmitz.home@googlemail.com>
2019-01-26 12:40:40 +0100

    Conversation structure

---
+++ Assets/Scripts/Conversation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Conversation", menuName = "Conversation")]
+public class Conversation : ScriptableObject
+{
+    public Choice m_StartChoice;
+}
+
+[CreateAssetMenu(fileName = "New Choice", menuName = "Choice")]
+public class Choice : ScriptableObject
+{
+    public string[] m_Options;
+    public Dialog[] m_Answers;
+    public Choice m_FollowUp;
+}
